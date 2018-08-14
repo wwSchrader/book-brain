7ca7ded129219011e60794ce5167b3d84633e67e
wwSchrader <wwschrader@gmail.com>
2018-08-13 21:48:13 -0700

    Move the Add A Book Button to the MyBooks component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,30 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 import NavigationBar from './NavigationBar';
 import LoginModal from './LoginModal';
-import {connect} from 'react-redux';
-import {addBookModalIsOpen} from '../redux/actions/index';
-import AddBookModal from './AddBookModal';
+
 import MyBooks from './MyBooks';
 import {Route, withRouter} from 'react-router-dom';
 import Home from './Home';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleAddBookClick = this.handleAddBookClick.bind(this);
-  }
-
-  handleAddBookClick() {
-    this.props.addBookModalIsOpen(true);
-  }
-
   render() {
     return (
       <div className="App">
         <NavigationBar/>
         <LoginModal/>
-        <AddBookModal/>
-        <button onClick={this.handleAddBookClick}>Add a Book</button>
         <Route exact path='/' component={Home} />
         <Route path='/mybooks' component={MyBooks} />
       </div>
@@ -33,10 +20,4 @@ class App extends Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addBookModalIsOpen: (isOpen) => dispatch(addBookModalIsOpen(isOpen)),
-  };
-};
-
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default App;
--- client/src/components/MyBooks.js
+++ client/src/components/MyBooks.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {addBookModalIsOpen} from '../redux/actions/index';
+import AddBookModal from './AddBookModal';
 
 class MyBooks extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAddBookClick = this.handleAddBookClick.bind(this);
+  }
+
+  handleAddBookClick() {
+    this.props.addBookModalIsOpen(true);
+  }
+
   render() {
     return (
-      <h3>My Books Screen</h3>
+      <div>
+        <h3>My Books Screen</h3>
+        <button onClick={this.handleAddBookClick}>Add a Book</button>
+        <AddBookModal/>
+      </div>
     );
   }
 }
 
-export default MyBooks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBookModalIsOpen: (isOpen) => dispatch(addBookModalIsOpen(isOpen)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(MyBooks);
