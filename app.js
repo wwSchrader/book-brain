31ffe395e387c0671209ba12efb58fe71c853457
wwSchrader <wwschrader@gmail.com>
2018-09-04 08:41:08 -0700

    Remove jade view engine

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ const index = require('./routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
