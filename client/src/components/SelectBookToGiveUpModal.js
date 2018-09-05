79d27f0a63ffbcf8bbb326a04880670105f7cd7a
wwSchrader <wwschrader@gmail.com>
2018-09-04 19:27:21 -0700

    Add user books to select book to give up modal

--- client/src/components/SelectBookToGiveUpModal.js
+++ client/src/components/SelectBookToGiveUpModal.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import {Modal} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {getUserBookArray} from '../redux/actions/index';
 
 class SelectBookToGiveUpModal extends Component {
+  componentDidMount() {
+    this.props.getUserBookArray();
+  }
+
   render() {
     return (
       <Modal show={this.props.showModal} onHide={this.props.onHide}>
@@ -9,11 +15,35 @@ class SelectBookToGiveUpModal extends Component {
           <Modal.Title>Choose Book To Give Up</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>BookId: {this.props.bookIdWanted}</p>
+          {this.props.bookArray.map((book) => {
+            return (
+              <div key={book._id}>
+                <h3>{book.bookTitle}</h3>
+                <img
+                    src={book.bookThumbnailUrl}
+                    alt={'Book cover of ' + book.bookTitle}
+                />
+                <button>Select</button>
+              </div>
+            );
+          })}
         </Modal.Body>
       </Modal>
     );
   }
 }
 
-export default SelectBookToGiveUpModal;
+const mapStateToProps = (state) => {
+  return {
+    bookArray: state.userBookArray,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getUserBookArray: () => dispatch(getUserBookArray()),
+  };
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps)(SelectBookToGiveUpModal);
